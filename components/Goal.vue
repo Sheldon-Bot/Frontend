8487507eb7a3141682b3c0c15e1c2e7cc9c8b8ff
mat4539 <mborg0020@gmail.com>
2021-03-05 00:34:31 -0500

    new Comps

---
+++ components/Goal.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="box">
+      <p class="initial"><span class="big-text">Our goal</span> was to build a robot that would allow for students online to have a physical presence within the classroom. It was roughly inspired by the robot made by Sheldon in "The Big Bang Theory".</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Goal"
+}
+</script>
+
+<style scoped>
+.box {
+  max-width: 75vh;
+  background-color: #559B81;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+.initial {
+  max-width: 70vh;
+  padding: 1em;
+  background-color: #243742;
+  color: aliceblue;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.big-text {
+  float: left;
+  font-size: 4em;
+}
+</style>
---
+++ components/PageHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <br><hr><br>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageHeader",
+  props: {
+    title: {
+      type: String,
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+    padding-top: 1em;
+    font-size: 36px;
+    color: aliceblue;
+    letter-spacing: 4px;
+  }
+  hr {
+    width: 60%;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    border: none;
+    height: 3px;
+    border-radius: 2px;
+    background-color: #35495e;
+  }
+</style>
