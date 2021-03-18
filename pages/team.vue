a74f97a6dd8223ea7b645cae82fdaf2224607f9b
mat4539 <mborg0020@gmail.com>
2021-03-17 22:39:22 -0400

    Update Titles

--- pages/team.vue
+++ pages/team.vue
@@ -2,13 +2,13 @@
   <div class="team">
     <PageHeader title="Our Team"/>
     <div class="rowAlignment">
-      <Member name="Mr. Haas" role="Instructor" :image="require(`~/assets/Mr. Haas.JPG`)"></Member>
-      <Member name="Mathew Borg" role="Project Manager" :image="require(`~/assets/Mathew Borg.JPG`)"></Member>
-      <Member name="Mason Chopik" role="Engineer" :image="require(`~/assets/Mason Chopik.JPG`)"></Member>
-      <Member name="Kaden Scott" role="Software Engineer" :image="require(`~/assets/Kaden Scott.JPG`)"></Member>
+      <Member name="Mr. Haas" role="Director of Operations" :image="require(`~/assets/Mr. Haas.JPG`)"></Member>
+      <Member name="Mathew Borg" role="General Engineer" :image="require(`~/assets/Mathew Borg.JPG`)"></Member>
+      <Member name="Mason Chopik" role="General Engineer" :image="require(`~/assets/Mason Chopik.JPG`)"></Member>
+      <Member name="Kaden Scott" role="Software Developer" :image="require(`~/assets/Kaden Scott.JPG`)"></Member>
       <Member name="Alex Schulz" role="Mechanical Engineer" :image="require(`~/assets/Alex Schulz.jpg`)"></Member>
-      <Member name="Ethan Kennedy Munsterman" role="Electrical Intern" :image="require(`~/assets/Ethan Kennedy Munsterman.JPG`)"></Member>
-      <Member name="Jordan Dolson" role="Co-Op Student" :image="require(`~/assets/Jordan Dolson.JPG`)"></Member>
+      <Member name="Ethan Kennedy Munsterman" role="Technical Staff" :image="require(`~/assets/Ethan Kennedy Munsterman.JPG`)"></Member>
+      <Member name="Jordan Dolson" role="Technical Staff" :image="require(`~/assets/Jordan Dolson.JPG`)"></Member>
     </div>
   </div>
 </template>
