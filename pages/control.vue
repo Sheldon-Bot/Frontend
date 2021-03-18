4e1d55601026a746a51e6a19a42e826dcc24f67c
mat4539 <mborg0020@gmail.com>
2021-03-18 12:40:37 -0400

    Remove unused code

--- pages/control.vue
+++ pages/control.vue
@@ -1,43 +1,24 @@
 <template>
-  <v-row justify="center" align="center">
     <div class="camera-row">
       <h1>Control</h1>
       <br><hr><br>
-      <CameraView class="camera-view" name="Front camera" url="http://158.69.116.172:5001/api/image"></CameraView>
-      <CameraView class="camera-view" name="Back camera" url="http://158.69.116.172:5001/api/imageBack"></CameraView>
+      <CameraView class="camera-view" name="Front camera" url="http://localhost:5001/image/front"></CameraView>
+      <CameraView class="camera-view" name="Back camera" url="http://localhost:5001/image/back"></CameraView>
     </div>
-    <div id="joystick" class="joystick"></div>
-  </v-row>
 </template>
 
 <script>
 import CameraView from "~/components/CameraView";
-import nipplejs from 'nipplejs';
+
 export default {
   components: {
     CameraView
   },
   data() {
-    return {
-      joy: null
-    }
-  },
-  mounted() {
-    const staticGamepad = nipplejs.create({
-      zone: document.querySelector('#joystick'),
-      mode: 'static',
-      position: { left: '50%', top: '50%' },
-      color: '#d77373',
-      threshold: 0.25,
-      fadeTime: 400,
-      maxNumberOfNipples: 1
-    })
-     this.joy = nipplejs.create({
-       zone: document.getElementById('joystick'),
-       color: 'blue'
-     });
+    return {}
   }
 }
+
 </script>
 
 <style scoped>
