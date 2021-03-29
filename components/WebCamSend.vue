5fac347c657a101fd7699a303eda494caef6210e
mat4539 <mborg0020@gmail.com>
2021-03-29 09:08:29 -0400

    Trying to send web cam data

---
+++ components/WebCamSend.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="webcam-container">
+    <img ref="webcam-out" class="webcam">
+    <div class="text-bar">
+      <h3 class="title">{{ name }}</h3>
+      <h3 class="status" ref="last-update-text">{{ lastUpdate }}</h3>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+const sendImage = document.createElement('~/assets/backendfeed.jpg')
+
+export default {
+  name: "WebCamSend",
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true
+    },
+  },
+
+  async refresh(){
+    if (process.client) {
+      const imageSend = this.$http.$post(this.url, {sendImage})
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+
+.webcam {
+  border-radius: 0.5em;
+  padding: 0.5em;
+  max-width: 20em;
+}
+.title,.status {
+  display: inline;
+  padding: 0.5em;
+}
+.title {
+  float: left;
+}
+.status {
+  float: right;
+}
+.webcam-container {
+  background-color: #353535;
+  border-radius: 0.5em;
+}
+
+</style>
