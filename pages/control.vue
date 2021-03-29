8fe367d5e20f8469b99e4e534a8bf98629ccdfd2
mat4539 <mborg0020@gmail.com>
2021-03-29 09:07:38 -0400

    Added WebCamSend

--- pages/control.vue
+++ pages/control.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="camera-row">
-      <h1>Control</h1>
-      <br><hr><br>
-      <CameraView class="camera-view" name="Front camera" url="http://localhost:5001/image/front"></CameraView>
-      <CameraView class="camera-view" name="Back camera" url="http://localhost:5001/image/back"></CameraView>
+    <div class="control-container">
+      <PageHeader title="Control"/>
+      <div class ="camera-row">
+        <CameraView class="camera-view" name="Front camera" url="http://localhost:5001/image/front"></CameraView>
+        <CameraView class="camera-view" name="Back camera" url="http://localhost:5001/image/back"></CameraView>
+        <WebCamSend class="camera-view" name="Web camera" url="http://localhost:5001/webcam"></WebCamSend>
+      </div>
     </div>
 </template>
 
@@ -34,12 +36,16 @@ export default {
 <style scoped>
 @media screen and (max-width: 992px) {
   .camera-row {
-    flex-direction: column;
+    flex-direction: row;
   }
 }
 .camera-row {
+  width: 100%;
   display: flex;
   flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 0.5em;
 }
 .camera-view {
   margin: 0.4em;
