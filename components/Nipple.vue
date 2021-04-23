5de321daa66f6c826b7a1f5d21cdb69940c4b8d5
mat4539 <mborg0020@gmail.com>
2021-04-23 00:04:34 -0400

    Trying to make nipple work2

--- components/Nipple.vue
+++ components/Nipple.vue
@@ -8,8 +8,22 @@
 import nipplejs from "nipplejs"
 
 export default {
-name: "Nipple"
+  name: "Nipple",
+  created() {
+    const options = {
+      zone: document.getElementById('joystick'),
+      color: 'blue',
+      size: 2,
+      position: {left: '50%', top: '50%'},    // preset position for 'static' mode
+      mode: 'static',                         // 'dynamic', 'static' or 'semi'
+      restJoystick: true,
+      shape: 'circle',                        // 'circle' or 'square'
+      dynamicPage: true,                      // Enable if the page has dynamically visible elements
+      follow: true,                           // Makes the joystick follow the thumbstick
+    };
 
+    const manager = nipplejs.create(options);
+  }
 }
 
 </script>
